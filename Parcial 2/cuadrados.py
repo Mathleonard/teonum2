ef1d8aa1cdf6ebb51370178e86856cf906332caa
Mathleonard <victor.castaneda@ciencias.unam.mx>
2023-11-03 14:27:36 -0600

    Finished project
    Co-authored-by: Ian P.C. <Ian326@users.noreply.github.com>

--- Parcial 2/cuadrados.py
+++ Parcial 2/cuadrados.py
@@ -12,6 +12,7 @@ def primos(numero):
         if len(lista_i) == 1:
             listaPrimos.append(i)
     #termina máquina de primos
+    listaPrimos.append(numero)
     return listaPrimos
 
 
@@ -48,12 +49,35 @@ def sumaDosCuadrados(numero):
     """Función auxiliar que calcula la suma de dos cuadrados"""
 
     mitad = numero // 2
-    raizEntera = int(mitad ** 0.5) +1
+    raizEntera = int(mitad ** 0.5) + 1
     for i in range(0,raizEntera+1):
         completo = (numero - (i ** 2)) ** 0.5
         if completo % 1 == 0:
             return i, int(completo)
 
+def sumaTresCuadrados(numero,listaPrimos):
+    """Función auxiliar que calcula la suma de tres cuadrados"""
+    tercia = numero // 3
+    raizEntera = int(tercia ** 0.5) + 1
+    for i in range(0,raizEntera+1):
+        nuevoNum = numero - (i ** 2)
+        listaFactorizacion = []
+        listaPrimos.append(nuevoNum)
+        factorizacionPrimos(nuevoNum,listaPrimos, listaFactorizacion)
+        if condicionDosCuadrados(listaFactorizacion) is True:
+            (d,e) = sumaDosCuadrados(nuevoNum)
+            return i, d, e
+    return None, None, None
+
+def sumaCuatroCuadrados(numero,listaPrimos):
+    """Función auxiliar que calcula la suma de cuatro cuadrados"""
+    cuarta = numero // 4
+    raizEntera = int(cuarta ** 0.5) + 1
+    for i in range(0,raizEntera+1):
+        nuevoNum = numero - (i ** 2)
+        (g,h,j) = sumaTresCuadrados(nuevoNum,listaPrimos)
+        if (g,h,j) != (None, None, None):
+            return i, g, h, j
 
 def cuadrados():
     """Función principal"""
@@ -78,8 +102,22 @@ def cuadrados():
     factorizacionPrimos(numero,listaPrimos, listaFactorizacion)
     if condicionDosCuadrados(listaFactorizacion) is False:
         print(f"{numero} no puede ser representado como suma de dos cuadrados.")
+
+        listaPrimosrep = listaPrimos[:]
+
+        if sumaTresCuadrados(numero,listaPrimosrep) == (None, None, None):
+            print(f"{numero} no puede ser representado como suma de tres cuadrados.")
+            (f,g,h,i) = sumaCuatroCuadrados(numero,listaPrimos)
+            print(f"{numero}={f}^2+{g}^2+{h}^2+{i}^2.") #4 cuadrados
+
+        else:
+            (c,d,e) = sumaTresCuadrados(numero,listaPrimos)
+            print(f"{numero}={c}^2+{d}^2+{e}^2.") #3 cuadrados
+            print(f"{numero}={c}^2+{d}^2+{e}^2+0^2.") #4 cuadrados
     else:
         (a,b) = sumaDosCuadrados(numero)
-        print(f"{numero}={a}^2+{b}^2.")
+        print(f"{numero}={a}^2+{b}^2.") #2 cuadrados
+        print(f"{numero}={a}^2+{b}^2+0^2.") #3 cuadrados
+        print(f"{numero}={a}^2+{b}^2+0^2+0^2.") #4 cuadrados
 
 cuadrados()
